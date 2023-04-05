21ec962763d6a926125192c9d11bb25e3dda28f7
minseo <raichal2000@naver.com>
2023-04-05 23:39:26 +0900

    draw font

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import pygame # pygame 모듈의 임포트
 import sys # 외장 모듈
 import argparse
-from pygame.locals import * # QUIT 등의 pygame 상수들을 로드한다.
 import Utils
+from pygame.locals import * # QUIT 등의 pygame 상수들을 로드한다.
+
 
 parser = argparse.ArgumentParser(description='2023-AI_huristic-201818779-전민서')
 parser.add_argument('--vertical', help='vertical,row')
@@ -11,14 +12,14 @@ args = parser.parse_args()
 
 #initial settings
 
-if len(args.vertical) != 0: #가로칸 개수
+if args.vertical is not None: #가로칸 개수
     vertical = int(args.vertical)
 else:
-    vertical = 30
-if len(args.stripe) != 0: #세로칸 개수
+    vertical = 5
+if args.stripe is not None: #세로칸 개수
     stripe = int(args.stripe)
 else:
-    stripe = 30
+    stripe = 5
 
 width = 600 # 상수 설정
 height = 600 # 상수 설정
@@ -34,15 +35,45 @@ pygame.display.set_caption('A* Search') # 창 제목 설정
 game_world = pygame.display.set_mode((width, height), 0, 32) # 메인 디스플레이를 설정한다
 clock = pygame.time.Clock() # 시간 설정
 
+#asset
+#star
+star_image = pygame.image.load('star.png')
+star = star_image.get_rect()
+star_image = pygame.transform.scale(star_image,(int(height/vertical),int(width/stripe) ))
+star.left = 0 #location
+star.top = 0 #location
+
+#door
+door_image = pygame.image.load('door.png')
+door = door_image.get_rect()
+door_image = pygame.transform.scale(door_image,(int(height/vertical),int(width/stripe) ))
+door.left = width-int(height/vertical) #location
+door.top = height-int(width/stripe) #location
+
+#Map
+grid = [[] for _ in range(vertical)]
+for i in range(vertical):
+    grid[i] = [[]for _ in range(stripe)]
+
+grid[2][2]='block'
+
 #game Loop
-
 while True: # 아래의 코드를 무한 반복한다.
-    for event in pygame.event.get(): # 발생한 입력 event 목록의 event마다 검사
-        if event.type == QUIT: # event의 type이 QUIT에 해당할 경우
-            pygame.quit() # pygame을 종료한다
-            sys.exit() # 창을 닫는다
+    event = pygame.event.poll() #이벤트 처리
+    if event.type == pygame.QUIT: #게임종료
+        break
+    elif event.type == pygame.MOUSEBUTTONDOWN: #마우스클릭
+        column_index = event.pos[0] # CELL_SIZE
+        row_index = event.pos[1] # CELL_SIZE
+        grid[int(row_index/int(height/stripe))][int(column_index/int(width/vertical))]='block'
+        print(grid)
     game_world.fill(white) # display를 하얀색으로 채운다
-    Utils.draw_grid(vertical,stripe,game_world,width,height)
-
+    Utils.draw_grid(game_world,width,height,vertical,stripe)
+    Utils.fill_block(game_world,width,height,vertical,stripe,grid)
+    game_world.blit(star_image,star)
+    game_world.blit(door_image,door)
     pygame.display.update() # 화면을 업데이트한다
     clock.tick(fps) # 화면 표시 회수 설정만큼 루프의 간격을 둔다
+
+
+pygame.quit()
